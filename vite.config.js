875800f3a1a3135e4e4d2c19897eb0c8645ea05f
Denis Gercek <denis.gercek@gmail.com>
2024-08-26 13:01:11 +0000

    fixed vite.config.js dir name

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/bouncing-ball-emdr/", // Replace <REPO_NAME> with your GitHub repository name
+  base: "/threejs-emdr/",
 });
